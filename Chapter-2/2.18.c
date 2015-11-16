be211ffc7a0ce80b1062e1c4571f0c0c8a6f6bbc
NoSoul <NoSoul.Love@GMail.com>
2015-11-16 15:20:54 +0800

    add problem 2.18

---
+++ Chapter-2/2.18.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%d\n", 0x1b8);
+    printf("%d\n", 0x14);
+    printf("%d\n", 0xfffffe58);
+    printf("%d\n", 0xfffffe74);
+    printf("%d\n", 0x44);
+    printf("%d\n", 0xfffffec8);
+    printf("%d\n", 0x10);
+    printf("%d\n", 0xc);
+    printf("%d\n", 0xfffffeec);
+    printf("%d\n", 0x20);
+    return 0;
+}
